5113780cce794b20ee6a87e2f6eaf9afbb095bca
Vincent Canfield <vc@cock.li>
2016-02-04 14:29:34 +0200

    Add composer to cap tasks, fix deploy directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@cock.li:cock.li.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/test.#{fetch(:application)}"
+set :deploy_to, "/var/www/test.#{fetch(:application)}/cap"
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -40,9 +40,9 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :composer, :install
+      end
     end
   end
 
