5693c1656bc84dc79315a5f0ebbee1544968b4b4
Vincent Canfield <vc@cock.li>
2016-03-01 21:38:19 +0200

    Having to make a commit every month for this is pretty gay tbh

--- resources/views/pages/donate.blade.php
+++ resources/views/pages/donate.blade.php
@@ -173,6 +173,7 @@
 	<li><a href="/donations/2015-11.txt">November 2015</a></li>
 	<li><a href="/donations/2015-12.txt">December 2015</a></li>
 	<li><a href="/donations/2016-01.txt">January 2016</a></li>
+	<li><a href="/donations/2016-02.txt">February 2016</a></li>
 </ul>
 
 @endsection
