2704c7e8f9f848a0ca9f725881eb3ff6bea127d1
Vincent Canfield <vc@cock.li>
2016-05-29 17:19:45 +0300

    add well-known to linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :linked_files, fetch(:linked_files, []).push('.env','storage/laravel.log')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('public/transparency','public/donations','storage/app','storage/logs','storage/framework')
+set :linked_dirs, fetch(:linked_dirs, []).push('public/transparency','public/donations','storage/app','storage/logs','storage/framework','public/.well-known')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
