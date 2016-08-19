03138b75e8d69f42c1d3cc67f58f393d97938b7c
Vincent Canfield <vc@cock.li>
2016-08-19 13:45:27 +0300

    Add donation data for July

--- resources/views/pages/donate.blade.php
+++ resources/views/pages/donate.blade.php
@@ -199,6 +199,7 @@
   <a class='col-xs-4 col-sm-2 col-lg-1' href="/donations/2016-04.txt">April</a>
   <a class='col-xs-4 col-sm-2 col-lg-1' href="/donations/2016-05.txt">May</a>
   <a class='col-xs-4 col-sm-2 col-lg-1' href="/donations/2016-06.txt">June</a>
+  <a class='col-xs-4 col-sm-2 col-lg-1' href="/donations/2016-07.txt">July</a>
 </strong></div>
 
 @endsection
