138e841c12747576fbf79c9ab3527fbff61f525a
Vincent Canfield <vc@cock.li>
2016-11-30 01:20:34 +0200

    Add piwik

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,6 +14,21 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+
+	<!-- piwik respects your freedoms don't have an autism -->
+	<script type="text/javascript">
+		var _paq = _paq || [];
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//stats.vc.gg/";
+			_paq.push(['setTrackerUrl', u+'piwik.php']);
+			_paq.push(['setSiteId', '1']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+<noscript><p><img src="//stats.vc.gg/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 </head>
 <body>
   <div class="container">
